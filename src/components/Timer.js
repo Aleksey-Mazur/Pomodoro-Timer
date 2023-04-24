9fa5a2191e0bbe22606341b33a46835caadb33ab
Aleksey <1aleksey.mazur@gmail.com>
2023-04-24 23:49:22 +0300

    layout changed; added Timer and Button components

---
+++ src/components/Timer.js
@@ -0,0 +1,29 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './PlayButton';
+import PauseButton from './PauseButton';
+
+const red = '#f54e4e';
+const green = '#4aec8c';
+
+const Timer = () => {
+  return (
+    <div>
+      <CircularProgressbar
+        value={60}
+        text={`${60}%`}
+        styles={buildStyles({
+          textColor: '#fff',
+          pathColor: red,
+          trailColor: 'rgba(255,255,255,.2)',
+        })}
+      />
+      <div style={{ marginTop: '1.25rem' }}>
+        <PlayButton />
+        <PauseButton />
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
